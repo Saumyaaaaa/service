ed99e84b827d7014199349cd44d338e5433a6e1c
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-04-15 11:34:53 +0545

    Added viewport meta tag and updated font imports in index.html; modified ServicesSection component styling and added hover sound effect

--- src/components/ServiceSection.jsx
+++ src/components/ServiceSection.jsx
@@ -1,14 +1,13 @@
-// import React from "react";
+
 import { LuArrowUpRight } from "react-icons/lu";
 
 const ServicesSection = () => {
   const playHoverSound = () => {
-    const hoverSound = new Audio("/click_002.ogg"); // Path to the sound file in the public folder
+    const hoverSound = new Audio("/click_002.ogg");
     hoverSound.currentTime = 0; // Reset sound to start
     hoverSound.play(); // Play sound
   };
 
-  
   const services = [
     {
       icon: "/computer.svg",
@@ -49,17 +48,12 @@ const ServicesSection = () => {
   ];
 
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto px-4 font-semibold">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {services.map((service, index) => (
           <div
             key={index}
-            className="group relative flex items-center gap-8 rounded-xl border bg-white px-4 py-6 transition-all duration-500 hover:-translate-y-2 cursor-pointer"
-            style={{
-              boxShadow: "rgba(0, 0, 0, 0.2) 0px 0px 20px",
-              fontFamily: "Inter",
-              position: "relative",
-            }}
+            className="group relative flex items-center gap-8 rounded-xl border bg-white px-4 py-6 transition-all duration-500 hover:-translate-y-2 cursor-pointer shadow-md"
             onMouseEnter={playHoverSound} // Call playHoverSound function on hover
           >
             <div className="rounded-2xl bg-blue-200 p-3">
@@ -72,7 +66,7 @@ const ServicesSection = () => {
             <span>{service.title}</span>
             {/* Arrow */}
             <div className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 mt-4 mr-4 ">
-              <LuArrowUpRight className="w-6 h-6 text-gray-500   group-hover:text-blue-500  translate-y-1 group-hover:translate-x-1" />
+              <LuArrowUpRight className="w-6 h-6 text-gray-500 group-hover:text-blue-500 translate-y-1 group-hover:translate-x-1" />
             </div>
           </div>
         ))}
